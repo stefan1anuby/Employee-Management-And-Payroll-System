de50a7a31725a581c8bdc6679c0a0b3909d171b7
unknown <geocaru@gmail.com>
2024-10-29 11:43:14 +0200

    Dependency Injection Pattern and Observer Pattern

--- Backend/server/src/main/java/com/uaic/server/entities/HumanResourcesEmployee.java
+++ Backend/server/src/main/java/com/uaic/server/entities/HumanResourcesEmployee.java
@@ -14,19 +14,24 @@ public class HumanResourcesEmployee extends Employee{
     
     List<Employee> employeesList;
     
+    
+    // Aici folosesc constructor injection, declarand dependenta List<Employee> ca parametru al contructorului.
     public HumanResourcesEmployee(Integer id, String name, String email, String role, String department, int salary, List<Employee> list) {
         super(id, name, email, role, department, salary);
         this.employeesList = list;
     }
 
+    
     public List<Employee> getEmployeesList() {
         return employeesList;
     }
 
+    // Aici folosesc setter injection, declarand dependenta List<Employee> ca parametru al setter-ului.
     public void setEmployeesList(List<Employee> employeesList) {
         this.employeesList = employeesList;
     }
     
+    // urmeaza sa folosesc Observer aici, pentru a notifica ceilalti angajati in legatura cu anunturile
     public void postAnnouncement()
     {
         
