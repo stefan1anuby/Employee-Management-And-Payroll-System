3c8885b1d9c4d79ce7324400c41aeab056a0762e
stefan1anuby <stefaneduard2002@gmail.com>
2024-11-02 13:12:06 +0200

    added dockerfile, stage env var and inmemorydb

--- Backend/server/src/main/java/com/uaic/server/config/DataSourceConfig.java
+++ Backend/server/src/main/java/com/uaic/server/config/DataSourceConfig.java
@@ -1,7 +1,6 @@
 package com.uaic.server.config;
 
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -13,26 +12,43 @@ public class DataSourceConfig {
 
     //TODO: look at EnvironmentConfig.java (and delete this)
     private void loadEnv(){
-        Dotenv dotenv = Dotenv.configure().directory("../../").load();
-        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        try{
+
+            Dotenv dotenv = Dotenv.configure().directory("../../").load();
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        }
+        catch(Exception e){
+            System.out.println("Coudn't read the .env file");
+        }
     }
 
     @Bean
     public DataSource dataSource() {
-
         // load env variables
         this.loadEnv();
 
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        String host = System.getProperty("POSTGRES_HOST");
-        String port = System.getProperty("POSTGRES_PORT");
-        String database = System.getProperty("POSTGRES_DB");
-        String username = System.getProperty("POSTGRES_USER");
-        String password = System.getProperty("POSTGRES_PASSWORD");
-        dataSource.setUrl("jdbc:postgresql://" + host + ":" + port + "/" + database);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        return dataSource;
+        String environment = System.getProperty("STAGE");
+        if (environment == null || environment.equalsIgnoreCase("test")) {
+            // Configure an in-memory H2 database for testing
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("org.h2.Driver");
+            dataSource.setUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+            dataSource.setUsername("sa");
+            dataSource.setPassword("");
+            return dataSource;
+        } else {
+            // Configure PostgreSQL database for other environments
+            DriverManagerDataSource dataSource = new DriverManagerDataSource();
+            dataSource.setDriverClassName("org.postgresql.Driver");
+            String host = System.getProperty("POSTGRES_HOST");
+            String port = System.getProperty("POSTGRES_PORT");
+            String database = System.getProperty("POSTGRES_DB");
+            String username = System.getProperty("POSTGRES_USER");
+            String password = System.getProperty("POSTGRES_PASSWORD");
+            dataSource.setUrl("jdbc:postgresql://" + host + ":" + port + "/" + database);
+            dataSource.setUsername(username);
+            dataSource.setPassword(password);
+            return dataSource;
+        }
     }
 }
