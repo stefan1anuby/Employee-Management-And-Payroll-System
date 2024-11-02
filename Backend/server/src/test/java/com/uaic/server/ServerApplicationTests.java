826db0c237ee26b5c2a948e347aaf8a580e3e500
unknown <geocaru@gmail.com>
2024-11-02 16:30:12 +0200

    rename the class

--- Backend/server/src/test/java/com/uaic/server/ServerApplicationTests.java
+++ Backend/server/src/test/java/com/uaic/server/ServerApplicationTests.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
-class ServerApplicatnionTests {
+class ServerApplicationTests {
 
       private InMemoryUserRepository userRepository;
 
