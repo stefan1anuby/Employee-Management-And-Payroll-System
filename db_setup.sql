8904b4d569d60240523d0f9922ee79645d353100
unknown <geocaru@gmail.com>
2024-11-08 13:35:14 +0200

    Added Departments table linked with Employees table

--- db_setup.sql
+++ db_setup.sql
@@ -8,6 +8,13 @@ CREATE TABLE Users (
     CreatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+
+CREATE TABLE Departments (
+    department_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    location VARCHAR(100)
+);
+
 CREATE TYPE employee_role AS ENUM ('Manager', 'HR');
 
 CREATE TABLE Employees (
@@ -16,8 +23,9 @@ CREATE TABLE Employees (
     phone_number VARCHAR(15),
     email VARCHAR(100) UNIQUE,
     role employee_role NOT NULL,
-    department VARCHAR(50),
     salary DECIMAL(10, 2),
     team VARCHAR(255),
-    managed_employee_ids INT[] DEFAULT ARRAY[]::INT[] 
+    managed_employee_ids INT[] DEFAULT ARRAY[]::INT[],
+    department_id INT REFERENCES Departments(department_id) ON DELETE SET NULL
 );
+
