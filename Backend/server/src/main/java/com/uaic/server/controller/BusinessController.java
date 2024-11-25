865cf2e65c6c4c074f7e3c2b82379dbd92424f79
George166382 <geocaru@gmail.com>
2024-11-25 21:30:46 +0200

    updated business controller

--- Backend/server/src/main/java/com/uaic/server/controller/BusinessController.java
+++ Backend/server/src/main/java/com/uaic/server/controller/BusinessController.java
@@ -1,7 +1,9 @@
 package com.uaic.server.controller;
 
 import com.uaic.server.entities.Business;
+import com.uaic.server.entities.Employee;
 import com.uaic.server.services.BusinessService;
+import com.uaic.server.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,9 @@ public class BusinessController {
 
     @Autowired
     private BusinessService businessService;
+
+    @Autowired
+    private EmployeeService employeeService;
 
     @GetMapping
     public ResponseEntity<Iterable<Business>> getBusinesses() {
@@ -53,6 +58,63 @@ public class BusinessController {
     public ResponseEntity<Void> deleteBusiness(@PathVariable Long id) {
         if (businessService.checkExistentBusinessById(id)) {
             businessService.deleteBusinessById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{businessId}/employees")
+    public ResponseEntity<Iterable<Employee>> getEmployeesByBusiness(@PathVariable Long businessId) {
+        Optional<Business> business = businessService.findBusinessById(businessId);
+        if (business.isPresent()) {
+            Iterable<Employee> employees = businessService.getEmployeesByBusiness(businessId);
+            return new ResponseEntity<>(employees, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/{businessId}/employees/{employeeId}")
+    public ResponseEntity<Employee> getEmployeeByBusiness(@PathVariable Long businessId, @PathVariable Integer employeeId) {
+        Optional<Employee> employee = businessService.getEmployeeByBusinessAndEmployeeId(businessId, employeeId);
+        if (employee.isPresent()) {
+            return new ResponseEntity<>(employee.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/{businessId}/employees")
+    public ResponseEntity<Void> addEmployeeToBusiness(@PathVariable Long businessId, @RequestBody Employee employee) {
+        Optional<Business> business = businessService.findBusinessById(businessId);
+        if (business.isPresent()) {
+            employee.setBusiness(business.get());
+            employeeService.createOrUpdateEmployee(employee);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/{businessId}/employees/{employeeId}")
+    public ResponseEntity<Void> updateEmployeeInBusiness(@PathVariable Long businessId, @PathVariable Integer employeeId, @RequestBody Employee employee) {
+        Optional<Business> business = businessService.findBusinessById(businessId);
+        if (business.isPresent() && employeeService.checkExistentEmployeeById(employeeId)) {
+            employee.setId(employeeId);
+            employee.setBusiness(business.get());
+            employeeService.createOrUpdateEmployee(employee);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{businessId}/employees/{employeeId}")
+    public ResponseEntity<Void> deleteEmployeeFromBusiness(@PathVariable Long businessId, @PathVariable Integer employeeId) {
+        Optional<Business> business = businessService.findBusinessById(businessId);
+        if (business.isPresent() && employeeService.checkExistentEmployeeById(employeeId)) {
+            employeeService.deleteEmployeeById(employeeId);
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
